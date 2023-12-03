bcaee0208d1c681ffb3ee7baa8240d976899b426
yousifadil <yousifadil16@gmail.com>
2023-12-03 16:23:26 +0300

    execve

--- excute_n_tokenize.c
+++ excute_n_tokenize.c
@@ -1,35 +1,46 @@
 #include "shell.h"
 
-extern char **environ;  // Access the current environment variables
+/**
+ * executes and tokenizes the commands
+ */
 
-void exe_commands(const char *command) {
-    pid_t pid = fork();
+void exe_commands(const char *command)
+{
+        pid_t pidd = fork();
 
-    if (pid == -1) {
-        perror("forking error");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // Parsing
-        char *args[128];
-        int argnum = 0;
-        char *token = strtok((char *)command, " ");
-        
-        while (token != NULL) {
-            args[argnum++] = token;
-            token = strtok(NULL, " ");
+        if (pidd == -1)
+        {
+                perror("forking error. \n");
+                exit(EXIT_FAILURE);
+        }
+        else if (pidd == 0)
+        {
+                /*parsing*/
+                char *args[128];
+                int argnum = 0;
+                char *token = strtok((char *)command, " ");
+                while (token != NULL)
+                {
+                        args[argnum++] = token;
+                        token = strtok(NULL, " ");
+                }
+
+                args[argnum] = NULL;
+
+                /*executing*/
+
+                if (execvp(args[0], args) == -1)
+                {
+                        perror("executing error \n");
+                        exit(EXIT_FAILURE);
+                }
+
         }
 
-        args[argnum] = NULL;
-
-        // Creating an array of environment variables
-        char *env[] = { "PATH=/bin:/usr/bin", NULL };  // Example environment variables
-
-        // Executing with execve
-        if (execve(args[0], args, env) == -1) {
-            perror("execve error");
-            exit(EXIT_FAILURE);
+        else
+        {
+                wait(NULL);
         }
-    } else {
-        wait(NULL);
-    }
 }
+~
+
