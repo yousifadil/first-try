4fea05bcd420c67012d908ce2fc6474a790e2f31
yousifadil <yousifadil16@gmail.com>
2023-12-03 17:07:05 +0300

    new feature

--- entry.c
+++ entry.c
@@ -11,11 +11,12 @@
 int main(void)
 {
 	char command[128];
+	char *args[128];
 
 	while (true) {
 		show_prompt();
 		check_commands(command, sizeof(command));
-		exe_commands(command, char *args);
+		exe_commands(command, args);
 	}
 	
 	return 0;
